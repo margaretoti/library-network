e38cdf35aff4e5e09f1ef6cab6e65a69b8bb27f6
Margaret <margaretoti@users.noreply.github.com>
2017-03-21 14:02:05 -0400

    Made suggested changes for PR1

--- spec/features/user_creates_new_book_spec.rb
+++ spec/features/user_creates_new_book_spec.rb
@@ -1,13 +1,19 @@
-require "rails_helper"
+require 'rails_helper'
 
 feature 'User creates a new book' do
   scenario 'successfully' do
     visit root_path
 
-    create_book("The Lord of the Rings: The Fellowship of the Ring", "J. R. R. Tolkien" )
+    create_book('The Fellowship of the Ring', 'J. R. R. Tolkien' )
 
-    expect(page).to have_css('.books li',
-                    text: 'The Lord of the Rings: The Fellowship of the Ring')
-    expect(page).to have_css('.books li', text: 'J. R. R. Tolkien')
+    expect(page).to have_css('.books', text: 'The Fellowship of the Ring')
+    expect(page).to have_css('.books', text: 'J. R. R. Tolkien')
   end
+end
+
+def create_book(title, author)
+  click_on "Add a new book"
+  fill_in "Title", with: title
+  fill_in "Author", with: author
+  click_on "Create Book"
 end

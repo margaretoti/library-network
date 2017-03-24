e7950bb206308aec04f42fe0ed13934e48fdd235
Margaret <margaretoti@users.noreply.github.com>
2017-03-24 12:56:11 -0400

    Refactor user edits book test

--- spec/features/user_edits_book_spec.rb
+++ spec/features/user_edits_book_spec.rb
@@ -3,16 +3,29 @@ require 'rails_helper'
 feature 'User edits an existing book' do
   scenario 'updates title field successfully' do
     @book = create(:book)
-    visit edit_book_path(@book)
-    fill_in "Title", with: "The Golden Compass"
-    click_button 'Save changes'
+    visit root_path
+
+    edit_book(@book, new_title: 'The Golden Compass',
+                     new_author: @book.author)
+
+    expect(page.current_path).to eq books_path
   end
 
-  # scenario 'updates title and author fields successfully' do
-  #   book = create(:book)
-  #   visit edit_book_path(book)
-  #   fill_in "Title", with: "The Amber Spyglass"
-  #   fill_in "Author", with: "Philip Pullman"
-  #   click_button 'Save changes'
-  # end
+  scenario 'updates title and author fields successfully' do
+    @book = create(:book)
+    visit root_path
+
+    edit_book(@book, new_title: 'The Amber Spyglass',
+                     new_author: 'Philip Pullman')
+
+    expect(page.current_path).to eq books_path
+  end
+
+  def edit_book(book, options={})
+    click_on 'Edit'
+    visit edit_book_path(book)
+    fill_in 'Title', with: options[:new_title]
+    fill_in 'Author', with: options[:new_author]
+    click_button 'Save changes'
+  end
 end

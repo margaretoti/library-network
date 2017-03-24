62c0eb507ef492fe48e4112f000d2a305ae6b4a3
Margaret <margaretoti@users.noreply.github.com>
2017-03-24 16:26:02 -0400

    Refactor book form into a partial, Add I18n

--- spec/features/user_creates_new_book_spec.rb
+++ spec/features/user_creates_new_book_spec.rb
@@ -6,8 +6,19 @@ feature 'User creates a new book' do
 
     create_book('The Fellowship of the Ring', 'J. R. R. Tolkien' )
 
+    expect(page).to have_content(t('books.index.header'))
+    expect(page).to have_content(t('books.create.flash_success'))
     expect(page).to have_css('.books', text: 'The Fellowship of the Ring')
     expect(page).to have_css('.books', text: 'J. R. R. Tolkien')
+  end
+
+  scenario "unsuccessfully - required field is missing" do
+    visit root_path
+
+    create_book('The Two Towers', '')
+
+    expect(page).to have_content(t('books.new.header'))
+    expect(page).to have_content(t('books.validations.required'))
   end
 end
 
@@ -15,6 +26,5 @@ def create_book(title, author)
   click_on t('books.index.add_new_book_link')
   fill_in t('simple_form.labels.title'), with: title
   fill_in t('simple_form.labels.author'), with: author
-  click_on 'Create Book'
-  expect(page.current_path).to eq books_path
+  click_button t('books.create.button')
 end
--- spec/features/user_deletes_book_spec.rb
+++ spec/features/user_deletes_book_spec.rb
@@ -5,11 +5,16 @@ feature 'User deletes a book', js: true do
     book = create(:book)
     visit root_path
 
-    expect {
-      accept_confirm 'Are you sure?' do
-        click_on 'Delete'
-      end
-    }.to change(Book, :count).by(-1)
-    expect(page.current_path).to eq root_path
+    delete_book
+
+    expect(page).to have_content(t('books.index.header'))
+    expect(page).to have_content(t('books.delete.flash_success'))
+    expect(page).not_to have_content(book.title)
+  end
+
+  def delete_book
+    accept_confirm t('books.delete.confirmation_text') do
+        click_on t('books.delete.link')
+    end
   end
 end

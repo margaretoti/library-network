dd4c5b52f4a094304f4ff680a589958cb78e8599
Margaret <margaretoti@users.noreply.github.com>
2017-03-24 11:35:24 -0400

    Add edit functionality to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,26 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def create
     @book = Book.create(book_params)
     redirect_to books_path
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(book_params)
+      redirect_to books_path
+    else
+      render 'edit'
+    end
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :author)
   end

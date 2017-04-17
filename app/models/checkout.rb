a10fa131aac3df844f256796ea699355574c2060
Margaret <margaretoti@users.noreply.github.com>
2017-04-17 16:05:12 -0400

    Fines for overdue books display on user profile

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,5 +1,6 @@
 class Checkout < ApplicationRecord
   CHECKOUT_PERIOD_IN_DAYS = 14
+  FINE_AMOUNT_PER_DAY_IN_CENTS = 0.10
 
   belongs_to :user
   belongs_to :book
@@ -9,5 +10,12 @@ class Checkout < ApplicationRecord
 
   def due_date
     self.created_at + CHECKOUT_PERIOD_IN_DAYS.days
+  end
+
+  def fine
+    days_overdue = (Time.zone.now - self.due_date).to_i/1.day
+    if days_overdue > 0
+      fine = FINE_AMOUNT_PER_DAY_IN_CENTS * days_overdue
+    end
   end
 end
---
+++ spec/features/user_sees_fine_amounts_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'date'
+
+feature 'User sees fine amounts for overdue books' do
+  scenario 'sucessfully' do
+    patron = create(:patron)
+    book = create(:book)
+    checkout = create(:checkout, user: patron, book: book)
+    visit profile_path(as: patron)
+    
+    date_when_book_is_one_day_overdue = DateTime.now + (Checkout::CHECKOUT_PERIOD_IN_DAYS + 1).days
+    Timecop.travel(date_when_book_is_one_day_overdue) do
+      visit profile_path(as: patron)
+    end
+
+    expect(page).to have_content('$0.10')
+    expect(page).to have_css('td', text: 'Fine')
+  end
+end
